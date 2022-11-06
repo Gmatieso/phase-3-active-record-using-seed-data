fa805110b2ff1184f87f298604ffb9bc267e25f8
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-06 16:40:59 +0300

    Added more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,8 @@
 Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
 Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
 Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
-puts "Hurrah we've populated our Data"
+Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
+Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
+Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
+Game.create(title: "Candy Crush Saga", platform: "Mobile", genre: "Puzzle", price: 0)
+puts "Hurrah we've populated our Data Successfully"
